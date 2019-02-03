08676ae9888c07c8943abe8ace43761bbd61d39a
임정현 <imjeonghyeon@imjeonghyeon-ui-macbookpro.local>
2019-02-03 20:48:50 +0900

    Initial Commit

---
+++ BoxOffice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoxOffice
+//
+//  Created by 임정현 on 03/02/2019.
+//  Copyright © 2019 임정현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
